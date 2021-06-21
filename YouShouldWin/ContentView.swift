640272d385c24e7f820d59a75e93c172c615ce80
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-06-21 11:47:32 +0700

    Initial Commit

---
+++ YouShouldWin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YouShouldWin
+//
+//  Created by Antarcticaman on 21/6/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
